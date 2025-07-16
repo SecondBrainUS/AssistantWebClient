c91f7eb8c4525337601d4488a12e43a9e06dfd30
Alex Nordstrom <alexanderjnordstrom@gmail.com>
2025-07-16 11:02:33 -0400

    Add Tailwind CSS integration and update styles
    - Introduced Tailwind CSS as a dependency in package.json and package-lock.json.
    - Created a new tailwind.config.js file for Tailwind configuration.
    - Updated Vite configuration to include the Tailwind CSS plugin.
    - Modified App.css and index.css to incorporate Tailwind's utility classes.
    - Added a new logo image to the public directory and updated the Navbar component to display it.

--- assistant-web-client-v2/src/components/Navbar.tsx
+++ assistant-web-client-v2/src/components/Navbar.tsx
@@ -9,9 +9,11 @@ const Navbar: React.FC = () => {
   const profileImg = useCachedImage(user?.picture ?? '', `profile-img-${user?.user_id}`);
   return (
     <nav className="sticky top-0 z-50 flex justify-between items-center p-2 bg-gray-800 text-white">
-      <div>
-      <FontAwesomeIcon icon={faUser} />
-      </div>
+        <div className="flex items-center">
+        <img src="../assets/mush-v2-logo.png" alt="Logo" className="h-7 mr-3 ml-2" />
+        <h1 className="text-lg">Second Brain</h1>
+        <FontAwesomeIcon icon={faUser} />
+        </div>
     </nav>
   )
 }
