84cc838e0b92bc67997f9710c41f1ac6d435963e
Alex Nordstrom <alexanderjnordstrom@gmail.com>
2025-07-22 18:38:33 -0400

    CSS navbar mods

--- assistant-web-client-v2/src/components/Navbar.tsx
+++ assistant-web-client-v2/src/components/Navbar.tsx
@@ -7,12 +7,15 @@ import { faUser } from '@fortawesome/free-solid-svg-icons';
 const Navbar: React.FC = () => {
   const { user } = useAuth();
   const profileImg = useCachedImage(user?.picture ?? '', `profile-img-${user?.user_id}`);
+  // 
   return (
-    <nav className="sticky top-0 z-50 flex justify-between items-center p-2 bg-gray-800 text-white">
+    <nav className="sticky top-0 z-50 flex p-2 text-white bg-gray-800 justify-between items-center">
         <div className="flex items-center">
-        <img src="./mush-v2-logo.png" alt="Logo" className="h-7 mr-3 ml-2" />
-        <h1 className="text-lg">Second Brain</h1>
-        <FontAwesomeIcon icon={faUser} />
+          <img src="./mush-v2-logo.png" alt="Logo" className="h-7 mr-3 ml-2" />
+          <h1 className="text-lg">Second Brain</h1>
+        </div>
+        <div className="flex items-center">
+          <FontAwesomeIcon icon={faUser} />
         </div>
     </nav>
   )
